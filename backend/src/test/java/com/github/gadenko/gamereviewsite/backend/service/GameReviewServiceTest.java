7fa528436ab00957b1078153444b85eccbf7930c
Gadenko <gadenko@gmx.net>
2022-05-18 12:56:08 +0200

    change @Test void getAllGameReviews()

--- backend/src/test/java/com/github/gadenko/gamereviewsite/backend/service/GameReviewServiceTest.java
+++ backend/src/test/java/com/github/gadenko/gamereviewsite/backend/service/GameReviewServiceTest.java
@@ -3,10 +3,12 @@ package com.github.gadenko.gamereviewsite.backend.service;
 import com.github.gadenko.gamereviewsite.backend.dto.CreateGameReviewDto;
 import com.github.gadenko.gamereviewsite.backend.model.GameReview;
 import com.github.gadenko.gamereviewsite.backend.repo.GameReviewRepo;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class GameReviewServiceTest {
 
@@ -16,19 +18,27 @@ class GameReviewServiceTest {
     @Test
     void getAllGameReviews() {
         //Given
-        when(gameReviewRepo.findAll()).thenReturn(List.of(
-                new GameReview("1", "TES5 Skyrim", "Hält der Titel was er verspricht?", "Was für eine Fantasy Welt"),
-                new GameReview("2", "X4: Foundations", "Hält der Titel was er verspricht?", "Schöner Weltraum Titel. Sehr viele möglickeiten der entfaltung im Space.")
-        ));
+       GameReview gameReview1 = GameReview
+               .builder()
+               .id("1234-test")
+               .title("TES5 Skyrim")
+               .headline("Hält der Titel was er verspricht?")
+               .gameDescription("Was für eine Fantasy Welt")
+               .build();
+       GameReview gameReview2 = GameReview
+               .builder()
+               .id("test-1234")
+               .title("X4: Foundations")
+               .headline("Hält der Titel was er verspricht?")
+               .gameDescription("Schöner Weltraum Titel. Sehr viele möglickeiten der entfaltung im Space.")
+               .build();
+
+        when(gameReviewRepo.findAll()).thenReturn(List.of(gameReview1,gameReview2));
         //When
         List<GameReview> actual = gameReviewService.getAllGameReviews();
-        List<GameReview> expected = List.of(
-                new GameReview("1", "TES5 Skyrim", "Hält der Titel was er verspricht?", "Was für eine Fantasy Welt"),
-                new GameReview("2", "X4: Foundations", "Hält der Titel was er verspricht?", "Schöner Weltraum Titel. Sehr viele möglickeiten der entfaltung im Space.")
-        );
         //Then
+        assertThat(actual, Matchers.containsInAnyOrder(gameReview1,gameReview2));
         verify(gameReviewRepo).findAll();
-        assertEquals(expected, actual);
     }
 
     @Test
