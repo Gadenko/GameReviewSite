105fc7afd2e995c5db36cb82a791ce6768debbda
Gadenko <gadenko@gmx.net>
2022-05-22 22:26:23 +0200

    change Header and picture

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,12 +4,14 @@ export default function Header() {
     return (
         <header className="Header">
             <nav className="Nav">
-                <a className="Link" href="/">Home</a>
-                <a className="Link" href="/#/addgamereview">AddGameReview</a>
+                <img className="Header-Picture"
+                     src={"https://png.pngtree.com/png-clipart/20200720/original/pngtree-g-gaming-logo-png-image_4783094.jpg"}
+                     alt="logo"/>
+                <ul className="nav-list">
+                    <a className="Link" href="/">Home</a>
+                    <a className="Link" href="/#/addgamereview">AddGameReview</a>
+                </ul>
             </nav>
-            <img className="Header-Picture"
-                 src={"https://img.freepik.com/vektoren-kostenlos/weinleseabzeichen-der-hand-die-joystickvektorillustration-haelt-rundes-etikett-mit-gamepad_74855-11224.jpg?t=st=1653239105~exp=1653239705~hmac=d536938f4c420a81b8469dbcd43580bb07c01de0a1a5a5e9fc8443c8143489fa&w=826"}
-                 alt="logo"/>
         </header>
     )
 }
