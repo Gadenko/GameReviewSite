100b8e43a472158206778423f79d8e1c1572e0c1
Gadenko <gadenko@gmx.net>
2022-06-01 10:12:33 +0200

    ADD Security Backend

--- backend/src/main/java/com/github/gadenko/gamereviewsite/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/github/gadenko/gamereviewsite/backend/security/config/SecurityConfig.java
@@ -50,5 +50,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
 }
