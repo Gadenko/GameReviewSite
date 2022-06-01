64f46950971f18e42258568ae1fc62933a7f397b
Gadenko <gadenko@gmx.net>
2022-06-01 11:30:10 +0200

    ADD Security Frontend

--- frontend/src/hook/useDetailedGameReview.tsx
+++ frontend/src/hook/useDetailedGameReview.tsx
@@ -1,13 +1,15 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {GameReview} from "../model/GameReview";
 import {toast} from "react-toastify";
 import {getGameReviewBy} from "../service/game-review-api";
+import {AuthContext} from "../context/AuthProvider";
 
 export default function useDetailedGameReview(){
-    const [detailedGameReview, setDetailedGameReview] = useState<GameReview>()
+    const [detailedGameReview, setDetailedGameReview] = useState<GameReview>();
+    const {token} =useContext(AuthContext);
 
     const getGameReviewById = (id:string) => {
-        getGameReviewBy(id)
+        getGameReviewBy(id, token)
             .then(data => setDetailedGameReview(data))
             .catch((error) => toast.error(error))
     }
